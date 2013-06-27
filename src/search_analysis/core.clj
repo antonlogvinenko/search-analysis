c326fdcb237c56c291b7ee9b6e2682acc02614c6
Anton Logvinenko <logvinenko@farpost.com>
2013-06-27 11:22:57 +1100

    master Hardcore heavy lifting

--- src/search_analysis/core.clj
+++ src/search_analysis/core.clj
@@ -1,7 +1,8 @@
 (ns search-analysis.core
   (:require [clojure.data.json :as json]
             [clojure.set :as set])
-  (:use [clojure.java.io :only (reader writer delete-file)]))
+  (:use [clojure.java.io :only (reader writer delete-file)]
+        [clojure.string :only [split]]))
 
 (defn debug [x] (println x) x)
 
@@ -36,29 +37,29 @@
                           )]
     (set/intersection modification indexes)))
 
-(def DOCS-CHANGES-INFO "docs-changes-info")
+(def DOCS-CHANGES-INFO "/Volumes/flash/docs-changes-info")
+(def DOCS-INFO "/Volumes/flash/docs-info")
 
-(defn get-modification-info [indexes json1 json2]
+(defn get-modification-info [writer indexes json1 json2]
   (let [changed (vec (changed-keys indexes (:attributes json1) (:attributes json2)))]
     (if (-> changed empty? not)
-      (spit DOCS-CHANGES-INFO
-            (apply str
-                   (into
-                    [(json1 :id) \tab (-> json1 :attributes :type first) \tab]
-                    (conj
-                     (vec (interpose \space changed))
-                     \newline)))
-            :append true))
+      (.write writer
+              (apply str
+                     (into
+                      [(json1 :id) \tab (-> json1 :attributes :type first) \tab]
+                      (conj
+                       (vec (interpose \space changed))
+                       \newline)))))
     (->> changed
          (map #(vector % 1))
          (into {}))))
 
-(defn diff-event-processor [indexes [json1 json2]]
+(defn diff-event-processor [writer indexes [json1 json2]]
   (cond
    (and (nil? json1) (nil? json2)) {:zen 1}
    (nil? json1) {:event-added 1}
    (nil? json2) {:event-deleted 1}
-   :else (let [info (get-modification-info indexes json1 json2)]
+   :else (let [info (get-modification-info writer indexes json1 json2)]
            (conj info
                  (if (empty? info)
                    {:event-not-modified 1}
@@ -91,20 +92,33 @@
         not-modified (get coll :event-not-modified 0)
         sum (+ modified deleted added)]
     (if (-> sum (/ 10000) zero?)
-      (println "Count: " count)))
-  coll)
+      (println "Count: " sum))
+    coll))
 
 (defn analyze [file]
   (spit DOCS-CHANGES-INFO "")
+  (spit DOCS-INFO "")
   (let [indexes (into #{} (read-index-map))]
-    (with-open [rdr (reader file)]
+    (with-open [rdr (reader file)
+                wrt (-> DOCS-CHANGES-INFO
+                        java.io.FileWriter.
+                        (java.io.BufferedWriter. 10000))]
       (->> rdr
            line-seq
            (map parse-row)
            (partition 2)
            (map verify-event)
            print-state-info
-           (map (partial diff-event-processor indexes))
+           (map (partial diff-event-processor wrt indexes))
            (apply merge-with +)
            show
-           (spit "docs-info")))))
+           (spit DOCS-INFO)))))
+
+
+(require '[clojure.java.io :as jio])
+
+;; (defn spit2 [f content]
+;;   (with-open [#^java.io.Writer w (apply jio/writer f options)]
+;;     (.write w (str content))))
+
+
